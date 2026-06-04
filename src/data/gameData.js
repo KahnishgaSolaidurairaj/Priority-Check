f72cdcfa90352217166a30438edd78520a110b36
KahnishgaSolaidurairaj <krishnakani19@gmail.com>
2026-06-04 14:44:43 -0500

    Hurdle instructions updated

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -1,18 +1,18 @@
 export const hurdles = [
   { title: 'First Hurdle',
-    text: 'CC Website crashes while halfway through the intake form.',
+    text: 'The Counceling Center Website crashes while you are halfway finished with the intake form. In order to get in-touch with the Counceling Center to make an appointment you must spend/compromise one of the options (Academics, Financial, Physical Health, Social, Approval)',
   },
   { title: 'Second Hurdle',
-    text: 'You call the CC front desk but the intake form password is wrong.',
+    text: 'You call the Counceling Center front desk to get the intake form password; it is wrong and you have to call again and are waiting/on hold for 5 minutes, you must spend/compromise one of your remaining options (Academics, Financial, Physical Health, Social, Approval)',
   },
   { title: 'Third Hurdle',
-    text: 'CC front desk cannot help due to lack of availability.',
+    text: 'The Counceling Center front desk can not help due to not having availability; refers you to outside services, you decide to wait to get an appointment at the Counceling Center, you must spend/compromise one of your remaining options (Academics, Financial, Physical Health, Social, Approval)',
   },
   { title: 'Fourth Hurdle',
-    text: 'Counsellor cancelled your appointment last minute.',
+    text: 'Your Counsellor cancelled your appointment last minute, you must reschedule to a new time and date. To make a new appointment you must spend/compromise one of your remaining options (Academics, Financial, Physical Health, Social, Approval)',
   },
   { title: 'Fifth Hurdle',
-    text: 'You were charged a no-show fee for an appointment.',
+    text: 'You are booked for an appointment that you were not aware of, and were charged a no-show fee. To pay the no-show fee you must spend/compromise one of your remaining options (Academics, Financial, Physical Health, Social, Approval)',
   },
 ];
 
